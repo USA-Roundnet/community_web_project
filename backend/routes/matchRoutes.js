8f2a59ef55c47c5dfbb2b8a7f953f6abbde83228
Izayah Gibson <gibson.1258@osu.edu>
2025-01-13 17:30:11 -0500

    Add match and team routes with basic setup

---
+++ backend/routes/matchRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const matchController = require('../controllers/matchController');
+const router = express.Router();
+
+
+module.exports = router;
---
+++ backend/routes/teamRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const teamController = require('../controllers/teamController');
+const router = express.Router();
+
+
+module.exports = router;
