2e4543c94213484dc3f7a781b944b20de7968507
Tahseen <tarobbani@gmail.com>
2025-04-01 16:48:04 -0500

    erased extra 'you are new and want to'

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,6 @@ const HomePage = () => {
         <Hero />
 
         <section className="max-w-7xl mx-auto px-4 py-8">
-            <h2 className="text-2xl font-semibold mb-4">You are new and want to:</h2>
             <Grid />
         </section>
       </div>
