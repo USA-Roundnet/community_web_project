2d6e173fd1e18b7fabfc8263a167d8d206c540f6
yifei <yifeizhaogege@gmail.com>
2025-05-28 20:22:29 -0400

    no nav on login related pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
 
 function App() {
-    const noLayout = ["/login", "/register", "/forgot-password"];
-    const showLayout = !noLayout.includes(window.location.pathname);
-
     return (
         <Router>
-            {showLayout && <Navbar />}
             <AppRoutes />
         </Router>
     );
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,33 +1,39 @@
 // src/routes/AppRoutes.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import LoginPage from '../pages/LoginPage';
-import CreateAccountPage from '../pages/CreateAccountPage';
-import HomePage from '../pages/HomePage';
-import TournamentsPage from '../pages/TournamentsPage';
-import CreateTournamentBasicInfo from '../pages/CreateTournamentBasicInfo';
-import CreateTournamentFormat from '../pages/CreateTournamentFormat';
-import CreateTournamentRegistration from '../pages/CreateTournamentRegistration';
+import LoginPage from "../pages/LoginPage";
+import CreateAccountPage from "../pages/CreateAccountPage";
+import HomePage from "../pages/HomePage";
+import TournamentsPage from "../pages/TournamentsPage";
+import CreateTournamentBasicInfo from "../pages/CreateTournamentBasicInfo";
+import CreateTournamentFormat from "../pages/CreateTournamentFormat";
+import CreateTournamentRegistration from "../pages/CreateTournamentRegistration";
 // import RankingsPage from '../pages/RankingsPage';
-import NotFoundPage from '../pages/NotFoundPage';
-import EventsPage from '../pages/EventsPage';
+import NotFoundPage from "../pages/NotFoundPage";
+import EventsPage from "../pages/EventsPage";
+import Layout from "../components/Outlet";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<CreateAccountPage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tournaments" element={<TournamentsPage />} />
-      <Route path="/tournaments/create" element={<CreateTournamentBasicInfo />} />
-      <Route path="/tournaments/create/format" element={<CreateTournamentFormat />} />
-      <Route path="/tournaments/create/registration" element={<CreateTournamentRegistration />} />
-      {/* <Route path="/rankings" element={<RankingsPage />} /> */}
+    return (
+         <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/tournaments" element={<TournamentsPage />} />
+                <Route path="/tournaments/create" element={<CreateTournamentBasicInfo />} />
+                <Route path="/tournaments/create/format" element={<CreateTournamentFormat />} />
+                <Route path="/tournaments/create/registration" element={<CreateTournamentRegistration />} />
+                <Route path="/events" element={<EventsPage />} />
+            </Route>
 
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
+            {/* Routes that DO NOT use the layout */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<CreateAccountPage />} />
+            <Route path="/forgot" element={<LoginPage />} />
+
+            {/* 404 Page */}
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    );
 };
 
 export default AppRoutes;
