de4903d385f018566c07178774d12be163970480
yifei <yifeizhaogege@gmail.com>
2025-05-28 22:04:32 -0400

    hero

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import Grid from "../components/Grid";
 
 const HomePage = () => {
     return (
-        <div className="w-screen min-h-screen bg-gray-50">
+        <div className="w-full flex flex-col items-center justify-evenly">
             <Hero />
-            <section className="max-w-7xl mx-auto px-4 py-8">
+            <section className="w-7/8 mt-10 mb-10 flex flex-col items-center text-black">
+                <h1 className="text-4xl">Events</h1>
                 <Grid />
             </section>
         </div>
