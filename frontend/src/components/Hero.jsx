51daeae606b8283cd4426ccfe11f269690b3c6ba
yifei <yifeizhaogege@gmail.com>
2025-06-02 13:50:37 -0400

    homepage done

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,14 +1,16 @@
-import hero from "/spikeball-default.jpg";
+import hero from "/who.webp";
 
 const Hero = () => {
     return (
         <div
-            className="relative mt-10 w-7/8 h-[50vh] bg-cover bg-center text-white flex flex-col items-center justify-center mx-auto rounded-md"
+            className="relative  w-7/8 h-[70vh] bg-cover bg-center text-white flex flex-col items-center justify-center mx-auto rounded-md"
             style={{ backgroundImage: `url(${hero})` }}
         >
             <div className="absolute inset-0 bg-black/50 rounded-md" />{" "}
             {/* semi-transparent overlay */}
-            
+            <div className="h-[50%] relative z-10 flex flex-col items-center justify-evenly">
+                
+            </div>
         </div>
     );
 };
